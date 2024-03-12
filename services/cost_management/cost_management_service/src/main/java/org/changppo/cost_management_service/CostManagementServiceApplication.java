43ec6d3ddcb52c091601909e54c50b909c58865b
Onji Kim <xodks672@gmail.com>
2024-03-13 00:12:09 +0900

    init : project structure

---
+++ services/cost_management/cost_management_service/src/main/java/org/changppo/cost_management_service/CostManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.changppo.cost_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostManagementServiceApplication.class, args);
+	}
+
+}
---
+++ services/cost_management/cost_management_service/src/test/java/org/changppo/cost_management_service/CostManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.changppo.cost_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
