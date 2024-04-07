5eea44a141a7acdcc00d3b6ef84d79cbb300d93a
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-04-07 18:44:18 +0900

    chore : 직접적으로 사용하지 않고 상속 받는 BusinessException들을 추상 클래스로 변경

--- services/cost_management/cost_management_service/src/main/java/org/changppo/cost_management_service/response/exception/member/MemberBusinessException.java
+++ services/cost_management/cost_management_service/src/main/java/org/changppo/cost_management_service/response/exception/member/MemberBusinessException.java
@@ -3,9 +3,13 @@ package org.changppo.cost_management_service.response.exception.member;
 import org.changppo.cost_management_service.response.exception.common.BusinessException;
 import org.changppo.cost_management_service.response.exception.common.ExceptionType;
 
-public class MemberBusinessException extends BusinessException {
+public abstract class MemberBusinessException extends BusinessException {
 
     public MemberBusinessException(ExceptionType exceptionType) {
         super(exceptionType);
+    }
+
+    public MemberBusinessException(ExceptionType exceptionType, Throwable cause) {
+        super(exceptionType, cause);
     }
 }
