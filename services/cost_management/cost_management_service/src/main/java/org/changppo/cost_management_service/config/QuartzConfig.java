a5629c83bae15085f50f76ab0dc4f16b5135e453
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-04-08 18:10:57 +0900

    feat : quartz 환경 구축

---
+++ services/cost_management/cost_management_service/src/main/java/org/changppo/cost_management_service/config/QuartzConfig.java
@@ -0,0 +1,72 @@
+package org.changppo.cost_management_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+public class QuartzConfig {
+
+    private final ApplicationContext applicationContext;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        AutoWiringSpringBeanJobFactory autoWiringSpringBeanJobFactory = new AutoWiringSpringBeanJobFactory();
+
+        autoWiringSpringBeanJobFactory.setApplicationContext(applicationContext);
+        schedulerFactoryBean.setJobFactory(autoWiringSpringBeanJobFactory);
+        schedulerFactoryBean.setOverwriteExistingJobs(true);
+        schedulerFactoryBean.setAutoStartup(true);
+        schedulerFactoryBean.setTransactionManager(platformTransactionManager);
+        schedulerFactoryBean.setQuartzProperties(quartzProperties());
+        return schedulerFactoryBean;
+    }
+
+    private Properties quartzProperties() {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("security/quartz.properties"));
+        Properties properties = null;
+
+        try {
+            propertiesFactoryBean.afterPropertiesSet();
+            properties = propertiesFactoryBean.getObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    public static class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+        private transient AutowireCapableBeanFactory beanFactory;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            beanFactory = applicationContext.getAutowireCapableBeanFactory();
+        }
+
+        @Override
+        protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+            final Object job = super.createJobInstance(bundle);
+            beanFactory.autowireBean(job);
+
+            return job;
+        }
+    }
+
+}
