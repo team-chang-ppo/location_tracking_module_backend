be5ef6bb0adc4e3e624c82a19271f6c292aa2920
jeyong <joon6093@naver.com>
2024-05-08 21:58:38 +0900

    feat : 편의성을 위해 local 환경에서 실행 시 batch meta 데이터 초기화 하도록 구현

--- services/account/account_server-batch/src/main/java/org/changppo/account/batch/config/DataSourceConfig.java
+++ services/account/account_server-batch/src/main/java/org/changppo/account/batch/config/DataSourceConfig.java
@@ -6,7 +6,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -35,5 +40,16 @@ public class DataSourceConfig {
     @Bean(DOMAIN_DATASOURCE)
     public DataSource domainDataSource() {
         return new LazyConnectionDataSourceProxy(new HikariDataSource(domainHikariConfig()));
+    }
+
+    @Profile("local")
+    @Bean
+    public DataSourceInitializer metaDataSourceInitializer() {
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(metaDataSource());
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(new ClassPathResource("sql/batch-postgresql.sql"));
+        dataSourceInitializer.setDatabasePopulator(databasePopulator);
+        return dataSourceInitializer;
     }
 }
