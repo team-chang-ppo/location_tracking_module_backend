7e74817084eed0d900b8e0803d0315ba65308df2
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-05-09 16:12:38 +0900

    chore : 사용하지 않는 dao 메서드 제거

--- services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/member/MemberRepository.java
+++ services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/member/MemberRepository.java
@@ -1,32 +1,20 @@
 package org.changppo.account.repository.member;
 
 import org.changppo.account.entity.member.Member;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {  //TODO. 복잡한 쿼리는 Querydsl로 변경
     Optional<Member> findByName(String name);
 
     @Query("select m from Member m join fetch m.memberRoles where m.name = :name")
     Optional<Member> findByNameWithRoles(@Param("name") String name);
 
-    @Query(value = "select * from member where name = :name", nativeQuery = true)
-    Optional<Member> findByNameIgnoringDeleted(@Param("name") String name);
-
     @Query("select m from Member m join fetch m.memberRoles where m.id = :id")
     Optional<Member> findByIdWithRoles(@Param("id") Long id);
-
-    @Query("select m from Member m where m.paymentFailureBannedAt is null and m.deletionRequestedAt is null")
-    Page<Member> findMembersForAutomaticPayment(Pageable pageable);
-
-    @Query("select m from Member m where m.deletionRequestedAt is not null and m.deletionRequestedAt <= :time and m.paymentFailureBannedAt is null")
-    Page<Member> findMembersForDeletion(@Param("time") LocalDateTime time, Pageable pageable);
 }
--- services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/payment/PaymentRepository.java
+++ services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/payment/PaymentRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {  //TODO. 복잡한 쿼리는 Querydsl로 변경
     void deleteAllByMemberId(Long memberId);
     Optional<Payment> findFirstByMemberIdOrderByEndedAtDesc(Long memberId);
     @Query("select new org.changppo.account.service.dto.payment.PaymentDto(p.id, p.amount, p.status, p.startedAt, p.endedAt, p.cardInfo, p.createdAt)" +
