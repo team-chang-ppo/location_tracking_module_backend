50c95eedce07bb399e59f43802888e259b6ed9cb
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-05-17 14:11:55 +0900

    feat : apikey에 관련한 JPQL을 Querydsl로 구현

---
+++ services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/apikey/QuerydslApiKeyRepository.java
@@ -0,0 +1,18 @@
+package org.changppo.account.repository.apikey;
+
+import org.changppo.account.service.dto.apikey.ApiKeyDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.time.LocalDateTime;
+
+public interface QuerydslApiKeyRepository {
+    Slice<ApiKeyDto> findAllByMemberIdOrderByAsc(Long memberId, Long firstApiKeyId, Pageable pageable);
+    void deleteAllByMemberId(Long memberId);
+    void banForCardDeletionByMemberId(Long memberId, LocalDateTime time);
+    void unbanForCardDeletionByMemberId(Long memberId);
+    void banApiKeysForPaymentFailure(Long memberId, LocalDateTime time);
+    void unbanApiKeysForPaymentFailure(Long memberId);
+    void requestApiKeyDeletion(Long memberId, LocalDateTime time);
+    void cancelApiKeyDeletionRequest(Long memberId);
+}
