65931051986c49bc27a43c493af865e20bde400d
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-05-17 14:29:24 +0900

    feat : payment에 관련한 JPQL을 Querydsl로 구현

---
+++ services/account/account_core/account_domain/src/main/java/org/changppo/account/repository/payment/QuerydslPaymentRepository.java
@@ -0,0 +1,14 @@
+package org.changppo.account.repository.payment;
+
+import org.changppo.account.entity.payment.Payment;
+import org.changppo.account.service.dto.payment.PaymentDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.Optional;
+
+public interface QuerydslPaymentRepository {
+    void deleteAllByMemberId(Long memberId);
+    Optional<Payment> findFirstByMemberIdOrderByEndedAtDesc(Long memberId);
+    Slice<PaymentDto> findAllByMemberIdAndStatusNotCompletedFree(Long memberId, Long lastPaymentId, Pageable pageable);
+}
